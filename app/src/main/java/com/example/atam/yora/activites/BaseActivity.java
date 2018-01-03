eb6b9edd94924b77e755c1a5a8ed91777160ec9f
kuvishar <kuvishar5367@gmail.com>
2018-01-03 09:14:14 -0800

    app part - 5 Adding Toolbar

--- app/src/main/java/com/example/atam/yora/activites/BaseActivity.java
+++ app/src/main/java/com/example/atam/yora/activites/BaseActivity.java
@@ -3,16 +3,30 @@ package com.example.atam.yora.activites;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import com.example.atam.yora.R;
 import com.example.atam.yora.infrastructure.YoraApplication;
 
 public abstract class BaseActivity extends AppCompatActivity {
     protected YoraApplication application;
+    protected Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         application = (YoraApplication) getApplication();
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+
+        toolbar = (Toolbar) findViewById(R.id.include_toolbar);
+
+        if (toolbar != null)
+            setSupportActionBar(toolbar);
+
     }
 }
--- app/src/main/java/com/example/atam/yora/activites/MainActivity.java
+++ app/src/main/java/com/example/atam/yora/activites/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.atam.yora.activites;
 
 import android.os.Bundle;
 
+import com.example.atam.yora.R;
+
 public class MainActivity extends BaseAuthenticatedActivity {
     @Override
     protected void onYoraCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Inbox");
     }
 }
